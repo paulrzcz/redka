77e04fddc2a5ed9d4a5db8e42b2439587fac0949
Pavel Ryzov <paul@paulrz.cz>
2024-05-15 06:50:40 +0300

    Buildable

--- src/Redka/Run.hs
+++ src/Redka/Run.hs
@@ -5,13 +5,15 @@ module Redka.Run (run) where
 import Redka.Import
 import Network.Socket (PortNumber)
 import Redka.Server.Core (server)
+import RIO.Partial (read)
 
 run :: RIO App ()
 run = do
   logInfo "Checking the input parameters!"
-  port <- appOptions <$> optionsPort
-  view appOptions >>= logInfo . displayShow
-  if optionsServer <$> view appOptions
+  app <- ask
+  let opts = appOptions app
+  let port = (read $ optionsPort opts) :: PortNumber
+  if optionsServer opts
     then server port
     else logInfo "Client mode is not implemented yet!"
   logInfo "Done!"
