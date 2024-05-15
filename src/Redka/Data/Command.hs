cdffb48ae2c519e9699faf1ed687342e428c8181
Pavel Ryzov <paul@paulrz.cz>
2024-05-16 01:30:40 +0300

    Parsing two commands

--- src/Redka/Data/Command.hs
+++ src/Redka/Data/Command.hs
@@ -9,9 +9,11 @@ import Redka.Import
 import Redka.Data.Response
 import Redka.Data.Rexp (crlf, RespExpr (RespStringError))
 
+import qualified Data.ByteString.UTF8 as BSU
 
 import Data.Attoparsec.ByteString hiding (takeTill)
-import Data.Attoparsec.ByteString.Char8 (char, takeTill)
+import qualified Data.Attoparsec.ByteString as DAB
+import Data.Attoparsec.ByteString.Char8 (takeTill, skipSpace, endOfLine, isEndOfLine)
 
 data RespCommand
     = CmdGet !ByteString
@@ -19,24 +21,42 @@ data RespCommand
     deriving (Show, Eq, Ord, Generic)
 
 parseCmd :: ByteString -> Either RespResponse [RespCommand]
-parseCmd cmds = Left $ RespReply (RespStringError "NotImplementedParser")
+parseCmd cmds = eitherToEither $ parseOnly (pipelineParser <* endOfInput) cmds
+
+eitherToEither :: Either String [RespCommand] -> Either RespResponse [RespCommand]
+eitherToEither (Left s) = Left $ RespReply (RespStringError ("unable to parse: " <> BSU.fromString s))
+eitherToEither (Right cmds) = Right cmds
+
+resultToEither :: Result [RespCommand] -> Either RespResponse [RespCommand]
+resultToEither (Done _ r) = Right r
+resultToEither f = Left $ RespReply (RespStringError ("unable to parse: " <> BSU.fromString (show f)))
+
+notImplemented :: ByteString -> Either RespResponse [RespCommand]
+notImplemented s = Left $ RespReply (RespStringError ("NotImplemented " <> s))
 
 --- Parser
 
+pipelineParser :: Parser [RespCommand]
+pipelineParser = many cmdParser
+
 cmdParser :: Parser RespCommand
-cmdParser
-    =   cmdGetParser
-    <|> cmdSetParser
+cmdParser = do
+    cmd <- cmdGetParser <|> cmdSetParser
+    endOfLine
+    return cmd
 
 cmdGetParser :: Parser RespCommand
 cmdGetParser = do
     _ <- string "GET"
-    _ <- char ' '
-    CmdGet <$> takeByteString
+    skipSpace
+    key <- DAB.takeTill isEndOfLine
+    return $ CmdGet key
 
 cmdSetParser :: Parser RespCommand
 cmdSetParser = do
     _ <- string "SET"
-    _ <- char ' '
+    skipSpace
     key <- takeTill (== ' ')
-    CmdSet key <$> takeByteString
+    skipSpace
+    val <- DAB.takeTill isEndOfLine
+    return $ CmdSet key val
